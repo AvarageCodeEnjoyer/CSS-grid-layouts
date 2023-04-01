2e81005cc65661933b34fbbdfc8055ddc6e02cd2
AvarageCodeEnjoyer <111468739+AvarageCodeEnjoyer@users.noreply.github.com>
2023-03-31 17:05:46 -0700

    Add REPO

---
+++ app.js
@@ -0,0 +1,21 @@
+
+
+
+
+/* function greeting() {
+  var input = prompt("What is your Name?")
+  console.log(`Hello ${input}`)
+} */
+
+
+// greeting()
+
+/* function multiply() {
+  let input = prompt("Enter Number")
+  for (let i = 1; i <= 10; i++) {
+    let result = input * i
+    console.log(`${input} * ${i} = ${result}`)
+  }
+} */
+
+// multiply()
